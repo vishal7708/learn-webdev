902aaeb52af26820776c6bcba56d0a1dbcc32f7e
Vishal Gupta <vishalgupta7708@gmail.com>
2022-06-19 12:10:11 +0530

    multiple theme added

--- dark-light-theme/script.js
+++ dark-light-theme/script.js
@@ -1,9 +1,31 @@
-let select = document.querySelector('select');
-let html = document.querySelector('html');
+let select = document.querySelector("select");
+let html = document.querySelector("html");
 
-
-function changeTheme(bgcolor, txtcolor) {
+function update(bgcolor, txtcolor) {
     html.style.backgroundColor = bgcolor;
     html.style.color = txtcolor;
+    
 }
-select.addEventListener('change', () => (select.value === 'black' ) ? changeTheme('black', 'white') : changeTheme('white', 'black'));
+
+
+select.addEventListener('change', () => {
+    let choice = select.value;
+
+    switch (choice) {
+        case 'black':
+            update('black', 'white');
+            break;
+        case 'white':
+            update('white', 'black');
+            break;
+        case 'blue':
+            update('blue', 'white')
+            break;
+        case 'green':
+            update('green', 'white')
+    }
+})
+
+
+
+// select.addEventListener('change', () => (select.value === 'black') ? update('black', 'white'): update('white', 'black'))
